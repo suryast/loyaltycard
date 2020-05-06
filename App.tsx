4bbe0bf9841e062b15b8ac59c6469c0f4d846ca9
Surya Setiyaputra <surya.setiyaputra@online-pajak.com>
2020-05-06 21:02:39 +1000

    Fixed warnings

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { Colors } from "react-native/Libraries/NewAppScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ color: "#fff" }}>Loyalty Card App by Code.Sydney</Text>
+      <Text style={{ color: "#fff" }}>Hello React Native</Text>
     </View>
   );
 }
